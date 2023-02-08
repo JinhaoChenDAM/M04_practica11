5ed80c9f423bccf847ca612165cb582dddda2dd8
GJaumeMurciano <gjaume22@jaumebalmes.net>
2023-02-08 20:47:18 +0100

    commit guille amb el main actualitzat

--- exercici1_B.py
+++ exercici1_B.py
@@ -1,5 +1,5 @@
-#Aquesta funció mostra per pantalla indicant el número més gran i el més petit
-#Si els dos números són iguals no es mostren per pantalla
+# Aquesta funció mostra per pantalla indicant el número més gran i el més petit
+# Si els dos números són iguals no es mostren per pantalla
 def granPetit(num1, num2):
     if num1 > num2:
         print('El número {num1} és més gran que el número {num2}.'.format(num1=num1, num2=num2))
--- exercici2_B.py
+++ exercici2_B.py
@@ -1,7 +1,6 @@
-#Aquesta funció mostra per pantalla si el número és parell o senar
+# Aquesta funció mostra per pantalla si el número és parell o senar
 def parellSenar(num1):
     if num1 % 2 == 0:
         print('El número {num1} és parell.'.format(num1=num1))
     else:
         print('El número {num1} és senar'.format(num1=num1))
-
